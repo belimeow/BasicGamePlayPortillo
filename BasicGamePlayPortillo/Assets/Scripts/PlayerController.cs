68b66959183f74929c4180fa5cada29132df11f7
belimeow <711592@student.dusd.net>
2023-03-09 11:41:09 -0800

    cat
    Today i was able to make a working game that had errors that i fixed and I am now starting the extra credit

--- BasicGamePlayPortillo/Assets/Scripts/PlayerController.cs
+++ BasicGamePlayPortillo/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10;
-
+    public float zRange = 10;
 
     public GameObject projectilePrefab;
     // Start is called before the first frame update
@@ -20,17 +21,28 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //keep the player in bounds
-        if(transform.position.x < xRange)
+        if(transform.position.x < -xRange)
         {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (transform.position.x < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+        if (transform.position.x > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Verticle");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
