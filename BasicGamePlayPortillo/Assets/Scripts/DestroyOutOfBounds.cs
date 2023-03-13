3a0d0a72a75e9ff5c6aeaff61604bcfa3fe6fc6b
belimeow <711592@student.dusd.net>
2023-03-13 13:05:09 -0700

    hi
    Finished the hard mode in which we have a score  and health

--- BasicGamePlayPortillo/Assets/Scripts/DestroyOutOfBounds.cs
+++ BasicGamePlayPortillo/Assets/Scripts/DestroyOutOfBounds.cs
@@ -7,11 +7,12 @@ public class DestroyOutOfBounds : MonoBehaviour
     private float topBound = 30;
     private float lowerBound = -10;
     private float sideBound = 30.0f;
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -23,16 +24,17 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
-            Debug.Log("GameOver");
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
         else if (transform.position.x > sideBound)
         {
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
         else if (transform.position.x < -sideBound)
         {
-            Debug.Log("Game Over");
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
     }
